53ae78fac4dda05b6ca899e2645a3ce9889a173f
Patrick Larson <palarson@cs.washington.edu>
2014-05-02 18:47:28 -0700

    Added test project

---
+++ PocketPickupTest/src/com/uwcse403/pocketpickup/test/AutomationTest.java
@@ -0,0 +1,14 @@
+package com.uwcse403.pocketpickup.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AutomationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
