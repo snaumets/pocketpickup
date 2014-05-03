a3ac92fff5a466709eda37d3f21485923ba1ac0b
snaumets <snaumets@gmail.com>
2014-05-02 22:24:16 -0700

    adding MainActivityTest

---
+++ PocketPickupTest/src/com/uwcse403/pocketpickup/test/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.uwcse403.pocketpickup.test;
+
+import android.test.ActivityTestCase;
+
+public class MainActivityTest extends ActivityTestCase {
+
+	public MainActivityTest() {
+		super();
+	}
+	
+	public void testPreConditions() {
+	    assertTrue(false); // fail on purpose
+	  } // end of testPreConditions() method definition
+	
+}
