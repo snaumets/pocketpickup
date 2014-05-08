2553f16b0bc4e63fcd349f9c44c7f753fa756443
Jacob Gile <jjgile@cs.washington.edu>
2014-05-07 18:23:03 -0700

    Added Parse package and Game class

---
+++ PocketPickup/src/com/uwcse403/pocketpickup/Game.java
@@ -0,0 +1,12 @@
+package com.uwcse403.pocketpickup;
+/**
+ * Stores information about an individual pickupgame
+ * 
+ * @author Jacob Gile
+ */
+public final class Game {
+	/*Name of the user who created the game*/
+	String creatorName;
+	/*Location*/
+	
+}
