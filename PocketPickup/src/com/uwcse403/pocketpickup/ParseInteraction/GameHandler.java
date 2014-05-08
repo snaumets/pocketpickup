b66bbd50d78efdd49a33d9931880350503b9dfb1
isaiahmathieu <isaiah.mathieu@gmail.com>
2014-05-07 18:50:56 -0700

    added game handler file

--- PocketPickup/src/com/uwcse403/pocketpickup/ParseInteraction/GameHandler.java
+++ PocketPickup/src/com/uwcse403/pocketpickup/ParseInteraction/GameHandler.java
@@ -1,5 +1,28 @@
 package com.uwcse403.pocketpickup.ParseInteraction;
 
-public class GameHandler {
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+import com.uwcse403.pocketpickup.Game;
 
+public class GameHandler {
+	
+	public static void createGame(Game g) {
+		ParseObject game = new ParseObject("Game");
+		// fill in all the setters
+		game.put("sport", "some sport");
+		game.put("creator", "someone");
+		game.put("location", new ParseGeoPoint(30, 70));
+		
+		game.saveInBackground(new SaveCallback() {
+			public void done(ParseException e) {
+				if (e == null) {
+					// successfully created game
+				} else {
+					// unable to create the game, alert user
+				}
+			}
+		});
+	} 
 }
