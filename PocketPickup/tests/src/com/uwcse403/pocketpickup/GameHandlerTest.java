509412428f7a706fdfcd34a59a7a30fca3019719
Jacob Gile <jjgile@cs.washington.edu>
2014-05-13 20:05:02 -0700

    Fixed typo in GameHandlerTest

---
+++ PocketPickup/tests/src/com/uwcse403/pocketpickup/GameHandlerTest.java
@@ -0,0 +1,14 @@
+package com.uwcse403.pocketpickup;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameHandlerTest {
+
+	@Test
+	public void testCreateGame() {
+		fail("Not yet implemented");
+	}
+
+}
