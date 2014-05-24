e6fb8e3bb18fa2989dc3dd1b435c8805bc219899
Kane Swanson <kane@d-69-91-176-172.dhcp4.washington.edu>
2014-05-23 21:11:37 -0700

    added basic info to help screen

--- PocketPickup/src/com/uwcse403/pocketpickup/HelpActivity.java
+++ PocketPickup/src/com/uwcse403/pocketpickup/HelpActivity.java
@@ -18,6 +18,7 @@ public class HelpActivity extends Activity {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.help, menu);
+		
 		return true;
 	}
 
