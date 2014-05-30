cb1ff5e0329479948c2e62a7b9b54851263a50f9
Patrick Larson <palarson@cs.washington.edu>
2014-05-30 14:56:00 -0700

    Fixed string references

--- PocketPickup/src/com/uwcse403/pocketpickup/FindGameActivity.java
+++ PocketPickup/src/com/uwcse403/pocketpickup/FindGameActivity.java
@@ -429,18 +429,10 @@ public class FindGameActivity extends Activity
 	}
 	
 	private String getDateButtonString(final Calendar date) {
-		if (date == null) {
-			return getResources().getString(R.string.select_end_date);
-		} else {
-			return DateFormat.getDateFormat(this).format(date.getTime());
-		}
+		return DateFormat.getDateFormat(this).format(date.getTime());
 	}
 	
 	private String getTimeButtonString(final Calendar date) {
-		if (date == null) {
-			return getResources().getString(R.string.select_time);
-		} else {
-			return DateFormat.getTimeFormat(this).format(date.getTime());
-		}
+		return DateFormat.getTimeFormat(this).format(date.getTime());
 	}
 }
