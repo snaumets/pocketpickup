bb54bdace3c467a7cb0a05d8589e844af3f9d3fa
Jacob Gile <jjgile@cs.washington.edu>
2014-06-03 18:42:51 -0700

    added tests for SportPreferencesHandler

--- PocketPickup/src/com/uwcse403/pocketpickup/ParseInteraction/SportPreferencesHandler.java
+++ PocketPickup/src/com/uwcse403/pocketpickup/ParseInteraction/SportPreferencesHandler.java
@@ -6,16 +6,39 @@ import java.util.Set;
 
 import android.util.Log;
 
+import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.uwcse403.pocketpickup.PocketPickupApplication;
 
 public class SportPreferencesHandler {
 	public static String LOG_TAG = "SportPreferencesHandler";
+	private static final SaveCallback DEFAULT_SAVE_CALLBACK = new SaveCallback() {
+		public void done(ParseException e) {
+			if (e == null) {
+				// successfully created game
+				Log.v(LOG_TAG, "Successfully saved game");
+			} else {
+				// unable to create the game, alert user
+				Log.e(LOG_TAG, "error saving game: " + e.getCode() + ": " + e.getMessage());
+			}
+		}
+	};
+	
 	/**
 	 * Replaces the String list of sport preferences with the Strings provided as arguments 
 	 * @param varargs of the String names of the sport preferences
 	 */
 	public static void setSportPreferences(String... strings) {
+		setSportPreferences(DEFAULT_SAVE_CALLBACK, strings);
+	}
+	
+	/**
+	 * Performs the same action as setSportPreferences with a provided callback
+	 * @param cb - Callback to perform after save completion
+	 * @param strings - varargs of the String names of the sport preferences
+	 */
+	public static void setSportPreferences(SaveCallback cb, String... strings) {
 		// first make sure these are valid sports by comparing them agains the BiMap
 		Set<String> sportsNames = PocketPickupApplication.sportsAndObjs.keySet();
 		List<String> newSportPreferences = new ArrayList<String>();
@@ -30,9 +53,22 @@ public class SportPreferencesHandler {
 		// now update the sport preferences
 		ParseUser currentUser = ParseUser.getCurrentUser();
 		currentUser.put(DbColumns.USER_SPORT_PREFERENCES, newSportPreferences);
-		currentUser.saveInBackground();
+		if(cb != null) {
+			currentUser.saveInBackground(cb);
+		} else {
+			try {
+				currentUser.save();
+			} catch (ParseException e) {
+				Log.e(LOG_TAG, "Failed to save sport preferences");
+				e.printStackTrace();
+			}
+		}
 	}
 	
+	/**
+	 * Returns a list of the current user's sport preferences
+	 * @return List of Strings representing preferred sports
+	 */
 	public static List<String> getSportPreferences() {
 		ParseUser currentUser = ParseUser.getCurrentUser();
 		List<String> sports =currentUser.getList(DbColumns.USER_SPORT_PREFERENCES);
--- PocketPickupTests/src/com/uwcse403/pocketpickup/test/SportPreferencesHandlerTest.java
+++ PocketPickupTests/src/com/uwcse403/pocketpickup/test/SportPreferencesHandlerTest.java
@@ -5,17 +5,34 @@ import java.util.List;
 import android.test.ApplicationTestCase;
 import android.util.Log;
 
+import com.parse.SaveCallback;
 import com.uwcse403.pocketpickup.PocketPickupApplication;
 import com.uwcse403.pocketpickup.ParseInteraction.SportPreferencesHandler;
 
+/**
+ * Test suite for SportPreferencesHandler
+ */
 public class SportPreferencesHandlerTest extends ApplicationTestCase<PocketPickupApplication> {
+	/**Log tag label for logging**/
 	public static final String LOG_TAG = "SportPreferencesHandlerTest";
+	/**Null callback so that we call setSportPreferences with thread blocking.
+	 * Necessary to avoid the syntactic ambiguity of a null argument next to varargs
+	 */
+	public static final SaveCallback NO_CALLBACK = null;
+	/**Identifying name for Soccer**/
+	public static final String SOCCER = "Soccer";
+	/**Identifying name for Basketball**/
+	public static final String BASKETBALL = "Basketball";
 	
+	/**
+	 * Default constructor
+	 */
 	public SportPreferencesHandlerTest() {
 		super(PocketPickupApplication.class);
 	}
 	
 	@Override
+	/**{@inherit-doc}**/
 	protected void setUp() throws Exception {
 		super.setUp();
 		createApplication();
@@ -26,23 +43,44 @@ public class SportPreferencesHandlerTest extends ApplicationTestCase<PocketPicku
 	}
 	
 	@Override
+	/**{@inherit-doc}**/
 	protected void tearDown() {
 		terminateApplication();
 	}
 	
-	public void testSetSportPreferences() {
-		SportPreferencesHandler.setSportPreferences("Soccer");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	/**
+	 * Tests that getSportPreferences returns a non-null list of sportPrefs
+	 */
+	public void testGetSportPreferences() {
 		List<String> sportPrefs = SportPreferencesHandler.getSportPreferences();
 		for (int i = 0; i < sportPrefs.size(); i++) {
 			Log.v(LOG_TAG, sportPrefs.get(i));
 		}
 	}
 	
+	/**
+	 * Tests that setSportPreferences returns with no error
+	 */
+	public void testSetSportPreferences() {
+		SportPreferencesHandler.setSportPreferences(NO_CALLBACK, "Soccer");
+	}
+	
+	/**
+	 * Tests that changes made by setSportPreferences are visible by getSportPreferences
+	 */
+	public void testChangeSportPreferences() {
+		SportPreferencesHandler.setSportPreferences(NO_CALLBACK, SOCCER, BASKETBALL);
+		List<String> sportPrefs = SportPreferencesHandler.getSportPreferences();
+		assertTrue(sportPrefs.size() == 2);
+		assertTrue(sportPrefs.contains(SOCCER));
+		assertTrue(sportPrefs.contains(BASKETBALL));
+		
+		//Empty the sport preferences
+		String[] noSports = new String[0];
+		SportPreferencesHandler.setSportPreferences(NO_CALLBACK, noSports);
+		List<String> emptyPrefs = SportPreferencesHandler.getSportPreferences();
+		assertTrue(emptyPrefs.isEmpty());
+	}
+
 	
 }
